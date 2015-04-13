abdfb1abbbe0deb4e96ba065ab4894ebc2677ce3
talentlei <chenlei_0630@163.com>
2015-04-13 21:44:20 +0800

    1-8.cpp

---
+++ 1-Arrays-and-String/1-8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+
+
+using namespace std;
+//give the api isSubstring(str,str)
+//use one call isSubstring to judge if s1 and s2 are rotation 
+bool isRotationg(string s1,string s2){
+	if(s1.size()!=s2.size()||s1.size()==0)
+		return false;
+	ds1= s1+s1;
+	return isSubstring(ds1,s2);			
+}
