0c1ce515f774a3c53e210c9eb35bfc9557a088b8
talentlei <chenlei_0630@163.com>
2015-07-08 22:53:26 +0800

    Create 3-1.cpp

---
+++ 3-Stack-and-Queue/3-1.cpp
@@ -0,0 +1,9 @@
+//Describe how you could use a single array to implement three stacks
+
+/*
+  Answer:  we can divide the array into three part with the same element.
+          for each part, we design a stack.
+      
+
+
+*/
