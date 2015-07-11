b48786f03b59fa8c835c47d0e7d2fb195a3daa43
talentlei <chenlei_0630@163.com>
2015-07-11 13:04:59 +0800

    Update 3-1.cpp

--- 3-Stack-and-Queue/3-1.cpp
+++ 3-Stack-and-Queue/3-1.cpp
@@ -3,7 +3,12 @@
 /*
   Answer:  we can divide the array into three part with the same element.
           for each part, we design a stack.
-      
-
 
 */
+class MutiStack{
+public:
+  explicit MutiStack(int n=3, int capacity);
+private:
+  vector<int> top;
+  vector<int> tail;
+}
