0959eaa4e8ff2b99338a433302eaaf4872aac6be
talentlei <chenlei_0630@163.com>
2015-07-12 18:21:13 +0800

    Update 5-2.cpp

--- 5-Bit-Manipulation/5-2.cpp
+++ 5-Bit-Manipulation/5-2.cpp
@@ -1,1 +1,15 @@
-
+void binary(double a){
+    if(a>1||a<0)
+        cout<< "ERROR" <<endl;
+    string bit("0.");
+    int k=0;
+    while(k<32&&a!=0.0){
+        a*=2;
+        if(a>=1.0){
+         bit.append("1");
+         a-=1;
+        }
+        else  bit.append("0");
+    }
+    if(a!=0.0) cout<<"ERROR"<<endl;
+}
