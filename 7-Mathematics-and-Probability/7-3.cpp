9682cbda9b9dc2bc1c09f321fcb01a73a250cca6
talentlei <chenlei_0630@163.com>
2015-07-13 19:43:47 +0800

    Update 7-3.cpp

--- 7-Mathematics-and-Probability/7-3.cpp
+++ 7-Mathematics-and-Probability/7-3.cpp
@@ -1,1 +1,13 @@
+const double EPS=1e-5;
+class Line{
+public:
+    explicit Line(double _slope, double _yintercept)
+        :slope(_slope),yintercept(_yintercept){}
+    bool intersect(Line l1){
+        return (slope-l1.slope>EPS)||(yintercept-l1.yintercept<EPS);
+    }
+private: 
+    double slope;
+    double yintercept;
 
+}
