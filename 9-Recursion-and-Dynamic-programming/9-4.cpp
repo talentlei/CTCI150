092524404794c81915bb1a116781fa16b86200b8
talentlei <chenlei_0630@163.com>
2015-07-15 20:37:18 +0800

    Update 9-4.cpp

--- 9-Recursion-and-Dynamic-programming/9-4.cpp
+++ 9-Recursion-and-Dynamic-programming/9-4.cpp
@@ -1,1 +1,18 @@
 
+template<class T>
+vector<vector<T>> res;
+vector<vector<T>> getAllSubset(vector<T>& nums){
+  vector<T> one;
+  getSubset(nums,one,0);
+}
+
+void  getSubset(vector<T>& nums , vector<T>&one, int pos){
+    if(pos==nums.size()){
+      res.push_back(one);
+      return;
+    }
+    one.push_back(nums[pos]);
+    getSubset(nums,one,pos+1);
+    one.pop_back();
+    getSubset(nums,one,pos+1);
+}
