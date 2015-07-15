f8bfaebb9d22827a24f113a431a33bd24afb4038
talentlei <chenlei_0630@163.com>
2015-07-15 16:34:23 +0800

    Update 9-1.cpp

--- 9-Recursion-and-Dynamic-programming/9-1.cpp
+++ 9-Recursion-and-Dynamic-programming/9-1.cpp
@@ -1,1 +1,12 @@
 
+int stairsWays(int n){
+  if(n<1) return 0;
+  vector <int > ways(n);
+  ways[0] = 1;
+  ways[1] = ways[0]+1;
+  ways[2] = ways[0]+ways[1]+1;
+  int i=3; 
+  while(i<n)
+    ways[i] = ways[i-1]+ways[i-2]+ways[i-3];
+  return ways[n-1];
+}
