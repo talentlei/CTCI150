3d98e9ba82acd3ecf44983098ed8287bb1d83503
talentlei <chenlei_0630@163.com>
2015-07-15 16:03:32 +0800

    Update singleton.cpp

--- 8-Object-Oriented-Design/singleton.cpp
+++ 8-Object-Oriented-Design/singleton.cpp
@@ -1,1 +1,19 @@
 
+class Singleton{
+private:
+    static Singleton* _singleton;
+    static std::mutex mtx;
+    Singleton(){};
+
+public :
+    Singleton* getInstance(){
+        if(_singleton==NULL){
+            mtx.lock();
+            if(_singleton==NULL){
+                _singleton = new Singleton();
+            }
+            mtx.unlock();
+        }
+        return _singleton;
+    }
+};
