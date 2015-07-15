8d7bdb1b1ddb03f4cf0dbc20be62c6b8180e8722
talentlei <chenlei_0630@163.com>
2015-07-15 21:08:35 +0800

    Update 9-5.cpp

--- 9-Recursion-and-Dynamic-programming/9-5.cpp
+++ 9-Recursion-and-Dynamic-programming/9-5.cpp
@@ -1,1 +1,28 @@
 
+vector<vector<char>> res;
+vector<vector<char>> permutations(string s){
+    sort(s.begin(),s.end());
+    vector<char> one;
+    vector<bool> base(s.size(),false);
+    dfs(s,0,one,base);
+    return res;
+}
+
+void dfs(string s, int dep , vector<char>& one,  vector<bool>& base){
+    if(dep==s.size()){
+        res.push_back(one);
+        return;
+    }
+    for(int i=0; i<s.size(); i++){
+        if(!base[i]){
+            base[i] = true;
+            one.push_back(s[i]);
+            dfs(s,dep+1,one,base)
+            one.pop_back();
+            base[i] = false;
+            
+            while(i+1<s.size()&&s[i+1]==s[i]) i++;
+        }
+        
+    }
+}
