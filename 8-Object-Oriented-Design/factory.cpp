f7fbcbb45c127fdbbd14c392ad6f3052a83d0454
talentlei <chenlei_0630@163.com>
2015-07-15 15:54:31 +0800

    Update factory.cpp

--- 8-Object-Oriented-Design/factory.cpp
+++ 8-Object-Oriented-Design/factory.cpp
@@ -1,1 +1,27 @@
 
+class Car{
+public : 
+  virtual show()=0;
+}
+
+class CarA:class Car{
+  void show(){
+    cout<< "i am carA" <<endl;
+  }
+}
+
+class CarB:class Car{
+  void show(){
+    cout<< "i am carB" <<endl;
+  }
+}
+
+class CarFactory{
+    static Car* buildCar(string type){
+      if(type=="CarA")
+        return new CarA();
+      else if(type=="CarB")
+        return new CarB();
+    }
+}
+
