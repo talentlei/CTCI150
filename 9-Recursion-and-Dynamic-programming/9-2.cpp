7bc38df8bc5c77dcd941b4781b17a84af6910a99
talentlei <chenlei_0630@163.com>
2015-07-15 18:49:12 +0800

    Update 9-2.cpp

--- 9-Recursion-and-Dynamic-programming/9-2.cpp
+++ 9-Recursion-and-Dynamic-programming/9-2.cpp
@@ -1,4 +1,15 @@
 
-int pathWaysByDP(int x, int y ){
 
+int pathWaysByDP(int x, int y ){
+  vector<vector<int > > vec(x+1,vector<int>(y+1,1));
+  for(int i=1; i<=x; i++){
+    for(int j=1; j<=y; j++){
+      vec[i][j] = vec[i-1][j]+vec[i][j-1];
+    }
+  }
+  return vec[x][y];
 }
+
+//follow up
+
+
