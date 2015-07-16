b1ca8b6e503f5295cb6bd3d01e907030a37341e3
talentlei <chenlei_0630@163.com>
2015-07-16 11:38:17 +0800

    Update 9-7.cpp

--- 9-Recursion-and-Dynamic-programming/9-7.cpp
+++ 9-Recursion-and-Dynamic-programming/9-7.cpp
@@ -1,1 +1,17 @@
 
+bool _painFill(vector<vector<char>>& screen,int x,int y,char ncolor,char ocolor){
+    if(x<0||x>=screen.size()||y<0||y>=screen[0].size())
+        return false;
+    if(sreen[x][y]==ncolor){
+        sreen[x][y]=ocolor;
+        _painFile(screen,x+1,y,ncolor,ocolor);
+        _painFile(screen,x-1,y,ncolor,ocolor);
+        _painFile(screen,x,y+1,ncolor,ocolor);
+        _painFile(screen,x,y-1,ncolor,ocolor);
+    }
+    return true;
+}
+bool painFill(vector<vector<char>>&screen,int x,int y, char ocolor){
+    if(screen[x][y]==ocolor) return false;
+    return _painFill(screen,x,y,sreen[x][y],ocolor);
+}
