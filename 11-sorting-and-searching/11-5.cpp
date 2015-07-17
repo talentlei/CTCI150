a792426c623666e802bc9b700d2463188935a296
talentlei <chenlei_0630@163.com>
2015-07-17 22:03:16 +0800

    Update 11-5.cpp

--- 11-sorting-and-searching/11-5.cpp
+++ 11-sorting-and-searching/11-5.cpp
@@ -1,1 +1,18 @@
-int 
+int findstr(const vector<string>& strs , string s){
+  int beg = 0;
+  int end = strs.size()-1;
+  while(beg<=end){
+    int mid = beg+(end-beg)/2;
+    if(strs[mid]==s) return mid;
+    else if(strs[mid]==""){
+      if(strs[beg]==s)
+        return beg;
+      beg++;
+    }else{
+      if(strs[mid]>s)
+        end--;
+      else beg++;
+    }
+  }
+  return -1;
+}
