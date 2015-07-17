6d59eebd4016b8ea1509e448593d320e925e38ff
talentlei <chenlei_0630@163.com>
2015-07-17 21:22:58 +0800

    Update 11-3.cpp

--- 11-sorting-and-searching/11-3.cpp
+++ 11-sorting-and-searching/11-3.cpp
@@ -1,1 +1,26 @@
 
+int findRotateIndex(const vector<int>& arr){
+  
+}
+int findEWithOutDuplication(const vector<int>& arr,int target){
+  int end = arr.size()-1;
+  int beg = 0,tail = end;
+  if(target == arr[tail]) return tail;
+  while(beg<=end){
+    int mid = beg+(end-beg)/2;
+    if(arr[mid]==target) return mid;
+    if(target<arr[tail]){
+      //right half
+      if(arr[mid]>=arr[tail]||arr[mid]<target)
+        beg = mid+1;
+      else end = mid-1;
+    }
+    else{
+      if(arr[mid]<=arr[tail]||arr[mid]>target)
+        end = mid-1;
+      else beg = mid+1;
+      
+    }
+  }
+  return -1;
+}
