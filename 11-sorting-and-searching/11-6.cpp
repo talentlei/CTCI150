4008bd72b898bd39f33758f966dedf844a59042f
talentlei <chenlei_0630@163.com>
2015-07-17 22:12:31 +0800

    Update 11-6.cpp

--- 11-sorting-and-searching/11-6.cpp
+++ 11-sorting-and-searching/11-6.cpp
@@ -1,1 +1,19 @@
 
+pair<int ,int > find(const vector<vector<int>>& matrix, int n){
+    pair<int,int> res;
+    if(matrix.size()==0 || matrix[0].size()==0)
+        return res;
+    int m = matrix.size(), n = matrix[0].size();
+    int i=0,j=n-1;
+    while(i<m&&j>=0){
+        if(matrix[i][j]== n){
+            res.first = i;
+            res.second = j;
+            return res;
+        }
+        if(matrix[i][j]>n)
+            j--;
+        else i++;
+    }
+    return res;
+}
