e0db4413a9e204364807aed79f4ab522c7e29fa3
talentlei <chenlei_0630@163.com>
2015-07-17 20:45:34 +0800

    Update 11-1.cpp

--- 11-sorting-and-searching/11-1.cpp
+++ 11-sorting-and-searching/11-1.cpp
@@ -1,1 +1,14 @@
 
+void mergeSortedArray(vector<int>& B, vector<int>& A,int n){
+  int m = B.size();
+  int pa = n-1, pb = m-1, aft=m+n-1;
+  while(pa>=0&&pb>=0){
+    if(A[pa]>B[pb])
+      A[aft--] = A[pa--];
+    else  A[aft--] = B[pb--];
+  }
+  if(pa<0)
+    while(pb>=0)
+      A[aft--] = B[pb--];
+  return;
+}
