35b3b8a6edc00ab47cf38c88cbc8aca65d9e9744
talentlei <chenlei_0630@163.com>
2015-07-17 20:50:12 +0800

    Update 11-2.cpp

--- 11-sorting-and-searching/11-2.cpp
+++ 11-sorting-and-searching/11-2.cpp
@@ -1,1 +1,10 @@
 
+bool cmp(string a, string b){
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
+    return a<b;
+}
+
+void sortAnagram(vector<string>& A){
+    sort(A.begin(),A.end(),cmp());
+}
