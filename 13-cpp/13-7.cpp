cb587d98db38c28a249c44bb627ffec44823f1e6
talentlei <chenlei_0630@163.com>
2015-07-18 10:01:24 +0800

    Update 13-7.cpp

--- 13-cpp/13-7.cpp
+++ 13-cpp/13-7.cpp
@@ -6,10 +6,15 @@ struct node{
   node(int _val)
     :val(_val),left(nullptr),right(nullptr){};
 }
+//may have cycle.  attention!!
+map<node*,node*> mp;
 node* copy(node* root){
   node * res=nullptr;
   if(root==nullptr) return res;
+  if(mp.count(root)>0)
+    return mp[root];
   res = new node(root->val);
+  mp[root] = res;
   res->left = copy(root->left);
   res->right = copy(root->right);
   return res;
