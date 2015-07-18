6aea748a4d083c1ed8af2e67f22c495802c76b66
talentlei <chenlei_0630@163.com>
2015-07-18 11:12:09 +0800

    Update 13-10.cpp

--- 13-cpp/13-10.cpp
+++ 13-cpp/13-10.cpp
@@ -1,1 +1,15 @@
 
+int** my2DAlloc(int rows, int cols){
+  if(rows<=0||cols<=0) 
+    return NULL;
+  int head = rows*sizeof(int*);
+  int data = rows*cols*sizeof(int);
+  int ** res = (int**)malloc(head+data);
+  if(res==NULL)
+    return res;
+  int* buf = (int*)(res+rows);
+  for(int i=0; i<rows; i++){
+    res[i] = buf+i*cols;
+  }
+  return res;
+}
