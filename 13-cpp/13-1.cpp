0269d69f2797d6b7cfd7775613f697c9ce164c9e
talentlei <chenlei_0630@163.com>
2015-07-18 09:02:22 +0800

    Update 13-1.cpp

--- 13-cpp/13-1.cpp
+++ 13-cpp/13-1.cpp
@@ -1,1 +1,19 @@
 
+void printLastK(string infile,int k){
+    if(k<=0) return ;
+    ifstream is(infile.c_str(),ios::in);
+    string line;
+    vector<string> vec(k,"");
+    int ind =0,cnt=0;
+    while(getline(is,line)){
+        ind = ind%k;
+        vec[ind++]=line;
+        cnt++;
+    }
+    if(cnt>=k){
+        for(int i=0; i<k; i++)
+            cout<< vec[(ind+i)%k]<<end;
+    }
+    for(int i=0; i<cnt; i++)
+        cout<<vec[i]<<endl;
+}
