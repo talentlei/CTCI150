cc1cae9c5bb1bc50ef5cfcdad160fa40cd5e8c3e
talentlei <chenlei_0630@163.com>
2015-07-18 09:57:10 +0800

    Update 13-7.cpp

--- 13-cpp/13-7.cpp
+++ 13-cpp/13-7.cpp
@@ -1,1 +1,16 @@
 
+struct node{
+  int val;
+  node* left;
+  node* right;
+  node(int _val)
+    :val(_val),left(nullptr),right(nullptr){};
+}
+node* copy(node* root){
+  node * res=nullptr;
+  if(root==nullptr) return res;
+  res = new node(root->val);
+  res->left = copy(root->left);
+  res->right = copy(root->right);
+  return res;
+}
