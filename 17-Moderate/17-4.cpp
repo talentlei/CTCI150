e6b747451682e65144d54633a26f78b701e11c80
talentlei <chenlei_0630@163.com>
2015-07-19 15:52:12 +0800

    Update 17-4.cpp

--- 17-Moderate/17-4.cpp
+++ 17-Moderate/17-4.cpp
@@ -1,1 +1,27 @@
 
+template<typename T>
+T max(const T& a, const T& b){
+  return (a+b+abs(a-b))/2;
+}
+int sign(int a){
+  return (a>>31)&1;
+}
+
+int flip(int bit){
+  return bit^1;
+}
+
+int getMax(int a,int b){
+  int c = a-b;
+  
+  int sa = sign(a);
+  int sb = sign(b);
+  int sc = sign(c);
+  
+  int use_sign_of_a = sa^sb;
+  int use_sign_of_c = flip(sa^sb);
+  
+  int k = use_sign_of_a*sa+use_sign_of_c*sc;
+  int q = flip(k);
+  return a*k+b*q;
+}
