f818fc61c4d22caad9311e7116edec57124262ba
talentlei <chenlei_0630@163.com>
2015-07-19 17:25:32 +0800

    Update 17-5.cpp

--- 17-Moderate/17-5.cpp
+++ 17-Moderate/17-5.cpp
@@ -1,1 +1,25 @@
 
+void MasterMind(string solution, string guess){
+  unordered_map<char> hits;
+  unordered_map<char> phits;
+  
+  int cnt=0;
+  for(int i=0; i<4; i++){
+    if(solution[i]==guess[i])
+      cnt++;
+    else{
+      hits[solution[i]]++;
+      phits.[guess[i]]++; 
+    }
+  }
+  int pcnt=0;
+  for(unordered_map<char>::iterator iter=phits.begin(); iter!=phits.end(); iter++){
+    if(hits.count(iter->first)!=0){
+      int dt = max(iter->second,hits[iter->first]);
+      pcnt +=  dt;
+    }
+  }
+  cout << "hits:"<<cnt<<endl;
+  cout <<"presudo hits:" <<pcnt<<endl;
+  
+}
