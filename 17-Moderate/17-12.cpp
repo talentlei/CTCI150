c230edc86a8970a05df172110b9437cf1b1bb827
talentlei <chenlei_0630@163.com>
2015-07-21 18:54:54 +0800

    Update 17-12.cpp

--- 17-Moderate/17-12.cpp
+++ 17-Moderate/17-12.cpp
@@ -1,1 +1,18 @@
 
+// 2sum
+//is there Duplication element?
+//assump having Duplication
+
+vector<pair<int,int>> 2Sum(vector<int>&nums, int target){
+  vector<pair<int,int>> res;
+  sort(nums.begin(),nums.end());
+  int first = 0, end= nums.size()-1;
+  while(first<end){
+    if(first==target-end)
+      res.push_back(make_pair(first,end));
+    else if(first > target-end)
+      end--;
+    else first++;
+  }
+  return res;
+}
