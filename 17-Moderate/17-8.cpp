52edcdae4015c0e7644c5b2c01d0f5bd880008ff
talentlei <chenlei_0630@163.com>
2015-07-21 18:09:17 +0800

    Update 17-8.cpp

--- 17-Moderate/17-8.cpp
+++ 17-Moderate/17-8.cpp
@@ -1,4 +1,13 @@
 
 int partialSum(const vector<int>& nums){
-
+    if(nums.size()==0)
+      throw exception("no element");
+    int tmp = 0,sum=nums[0];
+    for(auto : n){
+      tmp += n;
+      sum = max(sum,tmp);
+      tmp = max(tmp,0);
+    }
+    return sum;
+    
 }
