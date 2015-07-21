400d1ee1e499968c77470678b7863029467600cd
talentlei <chenlei_0630@163.com>
2015-07-21 19:00:43 +0800

    Update 17-12.cpp

--- 17-Moderate/17-12.cpp
+++ 17-Moderate/17-12.cpp
@@ -8,8 +8,11 @@ vector<pair<int,int>> 2Sum(vector<int>&nums, int target){
   sort(nums.begin(),nums.end());
   int first = 0, end= nums.size()-1;
   while(first<end){
-    if(first==target-end)
+    if(first==target-end){
       res.push_back(make_pair(first,end));
+      first++;
+      end--; //what if having Duplication
+    }
     else if(first > target-end)
       end--;
     else first++;
