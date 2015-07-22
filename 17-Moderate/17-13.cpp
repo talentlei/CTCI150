46d8dfaea9e2aeb8d7d2480e28fb618fc9a4d77e
talentlei <chenlei_0630@163.com>
2015-07-22 19:48:22 +0800

    Update 17-13.cpp

--- 17-Moderate/17-13.cpp
+++ 17-Moderate/17-13.cpp
@@ -1,1 +1,30 @@
 
+class BiNode{
+  public:
+    BiNode node1,nod2;
+    int data;
+    BiNode(int val):data(val){}；
+}
+
+BiNode* BST2DoubleList(BiNode* root){
+    BiNode* cur=root, *pre=NULL,*head=NULL;
+    stack<BiNode*> stk;
+    while(!stk.empty()&&cur!=NULL){
+      while(cur!=NULL){
+        stk.push(cur);
+        cur = cur->left;
+      }
+      cur = stk.top();
+      stk.pop();
+      if(pre==NULL){
+        head = cur;
+        pre = cur; 
+      }
+      else {
+        pre->node2 = cur;
+        cur->node1 = pre;
+      }
+      cur = cur->node2;
+    }
+    return head;
+}
