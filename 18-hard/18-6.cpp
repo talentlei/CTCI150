8d9c46918414337032dc9a902b3104fd36f68675
talentlei <chenlei_0630@163.com>
2015-07-23 15:36:22 +0800

    Update 18-6.cpp

--- 18-hard/18-6.cpp
+++ 18-hard/18-6.cpp
@@ -1,1 +1,13 @@
 
+//heap
+vector<int> topK(const vector<int>& array){
+  vector<int> res(array.begin(),array.begin()+10000000);
+  make_heap(res.begin(),res.end());
+  for(int i=1000000; i<array.size(); i++){
+    if(array[i]<res[0]){
+      pop_heap();
+      push_heap(array[i]);
+    }
+  }
+  return res;
+}
