718954b60cbcf4ad4e77bae5c7b10084e69b46bc
talentlei <chenlei_0630@163.com>
2015-07-23 09:20:38 +0800

    Update 18-3.cpp

--- 18-hard/18-3.cpp
+++ 18-hard/18-3.cpp
@@ -1,2 +1,17 @@
-
-int random
+template<class T>
+vector<T> randomSelection(vector<T>& array, int m){
+  int n = array.size();
+  if(m>n)
+    throw exception();
+  vector<T> res(m,0);
+  for(int i=0; i<n; i++){
+    if(i<m)
+      res[i] = array[i];
+    else{
+     int idx = rand()%(i+1) ;
+     if(idx<m)
+       res[idx] = array[i]
+    }
+  }
+  return res;
+}
